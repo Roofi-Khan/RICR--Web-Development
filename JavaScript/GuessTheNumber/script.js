21807c30e87b2413ab02e4672520d15d3f61a53e
Roofi-Khan <khanroofi2004@gmail.com>
2025-12-13 18:21:54 +0530

    Guess the number changes

--- JavaScript/GuessTheNumber/script.js
+++ JavaScript/GuessTheNumber/script.js
@@ -1,11 +1,12 @@
+const rn = Math.floor(Math.random() * 10 + 1);
+console.log(rn);
+
 function submit() {
   const num = document.getElementById("number").value;
   if (!/^(10|[1-9])$/.test(num)) {
     alert("Enter a valid number");
     return;
   }
-  const rn = Math.floor(Math.random() * 10 + 1);
-  console.log(rn);
 
   if (num > rn) {
     const l = document.getElementById("result");
@@ -13,21 +14,20 @@ function submit() {
     p.innerText = "OOPS! SORRY!!! TRY A SMALLER NUMBER.";
     l.appendChild(p);
     // alert("OOPS! SORRY!!! TRY A SMALLER NUMBER.")
-  }
-  if (num < rn) {
+  } else if (num < rn) {
     const l = document.getElementById("result");
     const p = document.createElement("p");
     p.innerText = "OOPS! SORRY!!! TRY A LARGER NUMBER.";
     l.appendChild(p);
     // alert("OOPS! SORRY!!! TRY A LARGER NUMBER.")
-  }
-  if (num === rn) {
+  } else {
     const l = document.getElementById("result");
     const p = document.createElement("p");
-    p.innerText = "OOPS! SORRY!!! TRY A SMALLER NUMBER.";
+    p.innerText = "CONGRATULATION.YOU DID IT";
     l.appendChild(p);
     // alert("CONGRATULATION.YOU DID IT");
   }
-
+  
   document.getElementById("number").value = "";
 }
+
