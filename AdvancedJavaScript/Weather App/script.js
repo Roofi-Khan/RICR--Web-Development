886682ed2a7b9008e56ceb251ad4546cd790be9b
Roofi-Khan <khanroofi2004@gmail.com>
2025-12-14 17:31:00 +0530

    Weather app

---
+++ AdvancedJavaScript/Weather App/script.js
@@ -0,0 +1,32 @@
+async function getWeather() {
+  const city = document.getElementById("city").value.trim();
+  const { lat, lon } = await getGeoLoc(city);
+  console.log(lat, lon);
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b398ef3ec42b5598c86536668d957067`
+  );
+  const data = await response.json();
+
+  document.getElementById("WeatherData").innerHTML = `
+            <div>
+            <p>Temperature:${data.main.temp - 273.14}</p>
+           <p>Humidity:${data.main.humidity}%</p>
+            <p>Description:${data.weather[0].description}</p>
+          </div>
+          <img src="https://openweathermap.org/img/wn/${
+            data.weather[0].icon
+          }@4x.png" alt="Weathericon"/>
+`;
+}
+
+async function getGeoLoc(city) {
+  console.log(city);
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=b398ef3ec42b5598c86536668d957067`
+  );
+  const data = await response.json();
+  const lat = data[0].lat;
+  const lon = data[0].lon;
+
+  return { lat, lon };
+}
