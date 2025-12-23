15da111d1400f5e6c3e81ecb584d7f7f4514dc8e
Roofi-Khan <khanroofi2004@gmail.com>
2025-12-24 00:59:08 +0530

    Electricity Bill Calculator

--- Assignments/ElectricityBill Calculator/script.js
+++ Assignments/ElectricityBill Calculator/script.js
@@ -1,30 +1,100 @@
 function calculate() {
   const units = Number(document.getElementById("units").value);
-  const l=document.getElementById("result");
-  const unit = document.createElement("div");
-  unit.classList.add("container", "d-flex", "w-25");
+  const res = document.getElementById("result");
+  const d = document.createElement("div");
+  d.classList.add("row", "rounded");
+  d.innerHTML = `<div class="col-6 border border-dark"><b>Units</b></div>
+          <div class="col-6 border border-dark"><b>Charge</b></div>`;
+  res.appendChild(d);
   let charge = 0;
   if (units <= 50) {
     charge = units * 0.5;
     const d1 = document.createElement("div");
-    d1.innerText = `${units}`;
-    const d2=document.createElement("div");
-    d2.innerText=`${charge}`;
-    unit.appendChild(d1);
-    unit.appendChild(d2);
-    l.appendChild(unit);
-  }
-  if (units <= 200) {
+    d1.classList.add("row", "rounded");
+    d1.innerHTML = `<div class="col-6 border border-dark">${units}</div>
+          <div class="col-6 border border-dark">${charge}</div>`;
+    res.appendChild(d1);
+  } else if (units <= 200) {
     charge = 50 * 0.5 + (units - 50) * 0.75;
-  }
-  if (units <= 450) {
+    const d1 = document.createElement("div");
+    d1.classList.add("row", "rounded");
+    d1.innerHTML = `<div class="col-6 border border-dark">${50}</div>
+          <div class="col-6 border border-dark">${50 * 0.5}</div>`;
+    res.appendChild(d1);
+
+    const d2 = document.createElement("div");
+    d2.classList.add("row", "rounded");
+    d2.innerHTML = `<div class="col-6 border border-dark">${units - 50}</div>
+          <div class="col-6 border border-dark">${(units - 50) * 0.75}</div>`;
+
+    res.appendChild(d2);
+  } else if (units <= 450) {
     charge = 50 * 0.5 + 150 * 0.75 + (units - 200) * 1.2;
+    const d1 = document.createElement("div");
+    d1.classList.add("row", "rounded");
+    d1.innerHTML = `<div class="col-6 border border-dark">${50}</div>
+          <div class="col-6 border border-dark">${50 * 0.5}</div>`;
+    res.appendChild(d1);
+
+    const d2 = document.createElement("div");
+    d2.classList.add("row", "rounded");
+    d2.innerHTML = `<div class="col-6 border border-dark">${150}</div>
+          <div class="col-6 border border-dark">${150 * 0.75}</div>`;
+    res.appendChild(d2);
+
+    const d3 = document.createElement("div");
+    d3.classList.add("row", "rounded");
+    d3.innerHTML = `<div class="col-6 border border-dark">${units - 200}</div>
+          <div class="col-6 border border-dark">${(units - 200) * 1.2}</div>`;
+    res.appendChild(d3);
   } else {
     charge = 50 * 0.5 + 150 * 0.75 + 250 * 1.2 + (units - 450) * 1.5;
-  }
-  console.log(charge);
-  const total = charge + charge * 0.2;
-  console.log(total);
 
-  
+    const d1 = document.createElement("div");
+    d1.classList.add("row", "rounded");
+    d1.innerHTML = `<div class="col-6 border border-dark">${50}</div>
+          <div class="col-6 border border-dark">${50 * 0.5}</div>`;
+    res.appendChild(d1);
+
+    const d2 = document.createElement("div");
+    d2.classList.add("row", "rounded");
+    d2.innerHTML = `<div class="col-6 border border-dark">${150}</div>
+          <div class="col-6 border border-dark">${150 * 0.75}</div>`;
+    res.appendChild(d2);
+
+    const d3 = document.createElement("div");
+    d3.classList.add("row", "rounded");
+    d3.innerHTML = `<div class="col-6 border border-dark">${250}</div>
+          <div class="col-6 border border-dark">${250 * 1.2}</div>`;
+    res.appendChild(d3);
+
+    const d4 = document.createElement("div");
+    d4.classList.add("row", "rounded");
+    d4.innerHTML = `<div class="col-6 border border-dark">${units - 450}</div>
+          <div class="col-6 border border-dark">${(units - 450) * 1.5}</div>`;
+    res.appendChild(d4);
+  }
+  const d5 = document.createElement("div");
+  d5.classList.add("row", "rounded");
+  d5.innerHTML = `<div class="col-6 border border-dark"><b>Total Units: ${units}</b></div>
+  <div class="col-6 border border-dark"><b>SubTotal: ${charge}</b></div>`;
+  res.appendChild(d5);
+
+  const d6 = document.createElement("div");
+  d6.classList.add("row", "rounded");
+  d6.innerHTML = `<div class="col-12 border border-dark text-center"><b>Surcharge Amount: ${
+    charge * 0.2
+  }</b></div>`;
+  res.appendChild(d6);
+
+  const d7 = document.createElement("div");
+  d7.classList.add("row", "rounded");
+  d7.innerHTML = `<div class="col-12 border border-dark text-center"><b>Grand Total: ${
+    charge + charge * 0.2
+  }</b></div>`;
+  res.appendChild(d7);
+}
+
+function reset() {
+  window.location.reload();
 }
