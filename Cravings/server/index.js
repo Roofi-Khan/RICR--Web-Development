0ad015ffb48117b2474a6f024d51f72147a6e7e5
Roofi-Khan <khanroofi2004@gmail.com>
2026-01-27 20:27:03 +0530

    Photo updation, install cloudinary

--- Cravings/server/index.js
+++ Cravings/server/index.js
@@ -1,43 +1,48 @@
-import dotenv from 'dotenv'
-dotenv.config();
-
-import express from 'express';
-import cors from 'cors';
-import connectDB from './src/config/db.js';
+import express from "express";
+import cors from "cors";
+import connectDB from "./src/config/db.js";
 import AuthRouter from "./src/routers/authRouter.js";
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
-import PublicRouter from "./src/routers/publicRouter.js"
-import UserRouter from "./src/routers/userRouter.js"
+import morgan from "morgan";
+import cloudinary from "./src/config/cloudinary.js";
+import cookieParser from "cookie-parser";
+import PublicRouter from "./src/routers/publicRouter.js";
+import UserRouter from "./src/routers/userRouter.js";
 
 const app = express();
 
-app.use(cors({origin:"http://localhost:5173", credentials:true}));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 
 app.use(cookieParser());
 app.use(morgan("dev"));
 
-app.use("/auth",AuthRouter);
-app.use("/public",PublicRouter);
-app.use("/user",UserRouter);
+app.use("/auth", AuthRouter);
+app.use("/public", PublicRouter);
+app.use("/user", UserRouter);
 
-app.get("/",(req,res)=>{
-    console.log("Server is Working");
+app.get("/", (req, res) => {
+  console.log("Server is Working");
 });
 
-app.use((err,req,res,next)=>{
-    const ErrorMessage = err.message|| "Internal Server Error";
-    const StatusCode = err.statusCode || 500;
+app.use((err, req, res, next) => {
+  const ErrorMessage = err.message || "Internal Server Error";
+  const StatusCode = err.statusCode || 500;
 
-    console.log("Error Found ", {ErrorMessage,StatusCode});
-    res.status(StatusCode).json({message: ErrorMessage});
-})
+  console.log("Error Found ", { ErrorMessage, StatusCode });
+  res.status(StatusCode).json({ message: ErrorMessage });
+});
 
-const port = process.env.PORT ||5000;
-app.listen(port,()=>{
-    console.log("Server Started at Port:",port);
-    
-    connectDB();
-})
+const port = process.env.PORT || 5000;
+app.listen(port, async () => {
+  console.log("Server Started at Port:", port);
+
+  connectDB();
+
+  try {
+    const res = await cloudinary.api.ping();
+    console.log("Cloudinary API is working:", res);
+  } catch (error) {
+    console.error("Error Connecting Cloudinary API:", error);
+  }
+});
